198c52b16ce19b2a9765ec070c14a05a96ae6297
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-09-27 00:15:45 +0930

    Add in recycling, greenwaste as alternating weeks from each other, and set a start date based on the council data.

--- transform.rb
+++ transform.rb
@@ -6,7 +6,26 @@ data["features"].each do |feature|
   feature["properties"] = {
     "rub_day" => props["PICKUPDAY"],
     "name" => props["SUMMARY"],
-    "rub_weeks" => 1
+    "rub_weeks" => 1,
+    "rub_start" => "2013-05-01"
   }
+end
+
+recycling_data = JSON.parse(IO.read('paerecyclingcollectionareas.geojson'))
+recycling_data["features"].each do |feature|
+  props = feature["properties"].dup
+
+  feature["properties"] = {
+    "rec_day" => props["PICKUPDAY"],
+    "name" => props["SUMMARY"],
+    "rec_weeks" => 2,
+    "rec_start" => "2013-05-01",
+
+    "grn_day" => props["PICKUPDAY"],
+    "name" => props["SUMMARY"],
+    "grn_weeks" => 2,
+    "grn_start" => "2013-05-07",    
+  }
+  data["features"] << feature
 end
 puts data.to_json
